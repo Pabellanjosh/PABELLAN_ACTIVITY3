67abd492330cc059d869ead1d5ca3655e8ef535b
Pabellanjosh <145018260+Pabellanjosh@users.noreply.github.com>
2023-09-28 23:02:57 +0800

    Add files via upload

---
+++ PABELLAN_MinSecConverter2.py
@@ -0,0 +1,7 @@
+
+time = int(input("Enter the time seconds:"))
+
+minutes = time // 60
+seconds = time % 60
+
+print(str(time) +"seconds is " + str (minutes) + "minutes and" + str (seconds) + "seconds.")
